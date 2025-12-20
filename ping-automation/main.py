2675b0698c86f71b01d7bea6342c245702afdc11
sheenubhat <sheenubhat39@gmail.com>
2025-12-20 16:46:15 +0530

    test

--- ping-automation/main.py
+++ ping-automation/main.py
@@ -1,43 +1,59 @@
 
 # simple host availability checker
 
+
+# import argparse
+from flask import Flask, jsonify, request 
 import subprocess
-import argparse
-from flask import Flask, jsonify 
-import threading
+# # import threading
 
 app = Flask(__name__)
 results = {}
 
-def pingHost(host):
+def ping_host(host):
     try:
         output = subprocess.run(
-            ["ping", "-c", "1", host],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL
+            ["ping",  host],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            timeout=5,
+            shell=True
             
         )
-        results[host]= "reachable" if output.returncode == 0 else "unreachable"
-    except Exception:
-        results[host] = "error"
+        return "reachable" if output.returncode == 0 else "unreachable"
+    except Exception as e:
+        return f"error: {e}"
 
 
-@app.route("/status")
+@app.route("/ping", methods=["POST"])
+def ping():
+    data = request.json
+    targets = data.get("targets", [])
+
+    for host in targets:
+        results[host] = ping_host(host)
+    
+
+    return jsonify(results), 200
+
+
+@app.route("/status", methods=["GET"])
 def status():
-    return jsonify(results)
+    return jsonify(results), 200
 
-def start_api():
-    app.run(host="0.0.0.0", port=8080)
+# def start_api():
+#     app.run(host="0.0.0.0", port=8080)
 
 #  main
 
 if __name__ =="__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--targets", nargs="+", required=True)
-    args = parser.parse_args()
+    print("Starting Ping Automation Service.......")
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--targets", nargs="+", required=True)
+    # args = parser.parse_args()
 
 
-    for t in args.targets:
-        pingHost(t)
 
-    threading.Thread(target=start_api).start()
+
+    # threading.Thread(target=start_api).start()
+    app.run(host="127.0.0.1", port=8080)
